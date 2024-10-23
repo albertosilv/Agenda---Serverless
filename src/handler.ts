82228a3d366aaa1e774d3a4a57b9775cdde970cf
Alberto Silva <alberto.silva5456@gmail.com>
2024-10-23 12:55:04 -0300

    feat: add createAgendamento and getAgendamentos functions to handler

--- src/handler.ts
+++ src/handler.ts
@@ -1,8 +1,18 @@
-export const hello = async (event: any) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Hello, Serverless!",
-    }),
-  };
+import { APIGatewayEvent, Context } from "aws-lambda";
+import { AgendamentoController } from "./agendamento/controller/AgendamentoController";
+
+const agendamentoController = new AgendamentoController();
+
+export const createAgendamento = async (
+  event: APIGatewayEvent,
+  context: Context
+) => {
+  return agendamentoController.createAgendamento(event, context);
+};
+
+export const getAgendamentos = async (
+  event: APIGatewayEvent,
+  context: Context
+) => {
+  return agendamentoController.getAgendamentos(event, context);
 };
