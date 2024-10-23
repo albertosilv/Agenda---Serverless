5a702f420fc784faf4b1deb4a3c3177663863793
Alberto Silva <alberto.silva5456@gmail.com>
2024-10-23 12:53:17 -0300

    feat: add validation for doctor data

--- src/agendamento/controller/AgendamentoController.ts
+++ src/agendamento/controller/AgendamentoController.ts
@@ -4,6 +4,7 @@ import { CreateAgendamentoDTO } from "../dto/CreateAgendamentoRequestDTO";
 import { CreateAgendamentoResponseDTO } from "../dto/CreateAgendamentoResponseDTO";
 import { IAgendaentoService } from "../service/IAgendamentoService";
 import { AgendamentoService } from "../service/AgendamentoService";
+import medicos from "../mock/medicoMock";
 export class AgendamentoController {
   private agendaService: IAgendaentoService;
 
@@ -14,13 +15,24 @@ export class AgendamentoController {
   public async createAgendamento(event: APIGatewayEvent, context: Context) {
     const agendamentoData: CreateAgendamentoDTO = JSON.parse(event.body!);
 
+    const medicoExistente = medicos.find(
+      (medico) => medico.id === agendamentoData.medico_id
+    );
+
+    if (!medicoExistente) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ mensagem: "Médico não encontrado." }),
+      };
+    }
+
     const newAgendamento =
       this.agendaService.createAgendamento(agendamentoData);
 
     const response: CreateAgendamentoResponseDTO = {
       mensagem: "Agendamento realizado com sucesso",
       agendamento: {
-        medico: "",
+        medico: medicoExistente.nome,
         paciente: agendamentoData.paciente,
         data_horario: newAgendamento.data_horario,
       },
